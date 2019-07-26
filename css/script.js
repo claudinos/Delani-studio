1d4a11ea6e1b647f5e48557fd2f55f8b09954611
claudinos <claudinos1990@gmail.com>
2019-07-26 15:35:52 +0200

    add home page

---
+++ css/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  $("#one").click(function(){
+    $("#one").hide();
+    $("#two").show();
+    
+  });
+  $("#two").click(function(){
+    $("#two").hide();
+    $("#one").show();
+    
+  });
+});
+$(document).ready(function(){
+  $("#three").click(function(){
+    $("#three").hide();
+    $("#four").show();
+    
+  });
+  $("#four").click(function(){
+    $("#four").hide();
+    $("#three").show();
+    
+  });
+});
+$(document).ready(function(){
+  $("#five").click(function(){
+    $("#five").hide();
+    $("#six").show();
+    
+  });
+  $("#six").click(function(){
+    $("#six").hide();
+    $("#five").show();
+    
+  });
+});
