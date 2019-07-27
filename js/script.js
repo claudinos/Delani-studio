6408e9e9799a204bad3fc92d4201c556ab3bc3f8
claudinos <claudinos1990@gmail.com>
2019-07-27 18:02:32 +0200

    update my message

--- js/script.js
+++ js/script.js
@@ -1,48 +1,49 @@
-$(document).ready(function(){
-  $("#one").click(function(){
+$(document).ready(function () {
+  $("#one").click(function () {
     $("#two").show();
     $("#one").hide();
-    
+
   });
-  $("#two").click(function(){
+  $("#two").click(function () {
     $("#one").show();
     $("#two").hide();
-    
+
   });
 });
-$(document).ready(function(){
-  $("#three").click(function(){
+$(document).ready(function () {
+  $("#three").click(function () {
     $("#four").show();
     $("#three").hide();
-    
+
   });
-  $("#four").click(function(){
+  $("#four").click(function () {
     $("#three").show();
     $("#four").hide();
-    
+
   });
-});
-$(document).ready(function(){
-  $("#five").click(function(){
+  $("#five").click(function () {
     $("#six").show();
     $("#five").hide();
-    
+
   });
-  $("#six").click(function(){
+  $("#six").click(function () {
     $("#five").show();
     $("#six").hide();
-    
+
+  });
+  $("#submit").click(function (come) {
+    var name = $("#name").val()
+    var email = $("#email").val()
+    var message = $("#message").val();
+    if ((name !== "") || (email !== "") || (message !== "")) {
+      alert(name + " we have received your message. Thank you for reaching out to us.");
+    }
+    else {
+      alert("fill the form!" );
+    }
+    come.preventDefault();
   });
 });
-$("#kabiri").submit(function(event){
-  var name=("input#name").val()
-  var email=("input#email").val()
-  var message=$.trim($("#message").val());
-  if((name!=="")||(email!=="")||(message!=="")){
-    alert("fill the form!");
-  }
-  else{
-    alert(name+"we have received your message. Thank you for reaching out to us. ")
-  }
-  event.preventDefault();
-});
+// $(document).ready(function(){
+
+// });
